7f3bd65ca09f5b5fef6e14a929771803c1f7ddb0
jbtrystram <blitzkrieg263@gmail.com>
2016-01-26 19:21:38 +0100

    virtualenv

--- client.py
+++ client.py
@@ -3,123 +3,34 @@
 #!/usr/bin/env python 
 
 #import tty, sys, termios
-import RPi.GPIO as GPIO
 from time import sleep
 import os
 import websocket
 
-port = '50003'
+port = '50000'
 dest = '10.0.0.1'
 
-GPIO.setmode(GPIO.BOARD)
-GPIO.setup(7,GPIO.OUT) # LED OK
-GPIO.setup(22,GPIO.OUT) # Gauche (TI)
-GPIO.setup(18,GPIO.OUT) # Droite (TA)
-GPIO.setup(16,GPIO.OUT) # Recv (centre vert)
+def ping(host):
+    con = os.system('ping -c 1 ' + host)
+    if (con == 0):
+        return True
+    else:
+        return False
 
-GPIO.setup(15,GPIO.IN) #bouton droit
-GPIO.setup(13,GPIO.IN) #bouton gauche
+def isValidIP(addr):
+    try:
+        socket.inet_aton(addr)
+        return addr
+    except socket.error:
+        help("noServerIp")
 
-#eteindre tout
-GPIO.output(7, False)
-GPIO.output(22, False)
-GPIO.output(18, False)
-GPIO.output(16, False)
+def isValidDomain(addr):
+    try:
+        socket.inet_aton(addr)
+        return addr
+    except socket.error:
+        help("noServerIp")
 
-
-def ping (host):
-	con = os.system('ping -c 1 '+host)
-	if (con == 0): return True
-	else : return False
-
-
-def blink_receiv(rcv):
-    GPIO.output(16, False)
-    sleep(0.25)
-    GPIO.output(16, True)
-    sleep(0.15)
-    GPIO.output(16, False)
-    sleep(0.25)
-    GPIO.output(16, True)
-    sleep(0.15)
-    GPIO.output(16, False)
-    sleep(0.20)
-    if (rcv) : GPIO.output(16, True)
-
-
-
-def switch_led_Ti() :
-    GPIO.output(22, not GPIO.input(22))
-
-def switch_led_Ta() :
-    GPIO.output(18, not GPIO.input(18))
-
-def led_output(word):
-    if (word == 'ti') : switch_led_Ti()
-    elif (word == 'ta') : switch_led_Ta()
-    elif (word == 'slash'):
-        switch_led_Ti()
-        switch_led_Ta()
-    sleep(0.30)
-    if (word == 'ti') : switch_led_Ti()
-    elif (word == 'ta') : switch_led_Ta()
-    elif (word == 'slash'):
-        switch_led_Ti()
-        switch_led_Ta()
-
-def sendMorse (ws):
-    ws.send('ready to send')
-    leave=""
-    while (leave != "switch") :
-        if (leave == 'q') : quit()
-        if GPIO.input(13):
-            sleep(0.09)
-            if (GPIO.input(15) and GPIO.input(13)):
-                ws.send('slash')
-                led_output('slash')
-                leave = ws.recv() 
-            else:
-                ws.send('.')
-                led_output('ti')
-                leave = ws.recv() 
-                       
-        elif GPIO.input(15):
-            sleep(0.09)
-            if (GPIO.input(15) and GPIO.input(13)):
-                ws.send('slash')
-                led_output('slash')
-                leave = ws.recv() 
-            else:
-                ws.send('_')
-                led_output('ta')
-                leave = ws.recv()
-    print ("switch recevied L88")
-    blink_receiv(True)
-    receiveMorse(ws)
-
-def receiveMorse(ws) :
-    message = ""
-    ws.send('ready to rcv')
-    while (message != 'q'):
-        print ('waiting msg')
-        message = ws.recv()
-        if (message == 'switch') :
-            print ("switch recevied!")
-            blink_receiv(False)
-            sendMorse(ws)
-        else :
-            print (format(message)),
-            led_output(format(message))
-    ws.close()
-    quit()
-
-def quit() :
-    GPIO.output(7, False)
-    GPIO.output(22, False)
-    GPIO.output(18, False)
-    GPIO.output(16, False)
-    os.system('halt')
-    exit(0)
 
 if __name__ == "__main__":
 	
@@ -127,27 +38,50 @@ if __name__ == "__main__":
     while (i<3):
         if ping(dest) : 
             i=i+1
-            GPIO.output(7, True)
-            sleep(0.08*i)
-            GPIO.output(7,False)
         else : i=0
 
-    GPIO.output(7, True)
+    print("connected and ready")
     sleep(0.5)
-    GPIO.output(7,False)
 
     websocket.enableTrace(True)
     ws = websocket.create_connection("ws://"+dest+":"+port+"/websocket")
     
     #socket is established ask for the operating mode
     ws.send("Hello")
-    #wait for the answer
-    instruction = ws.recv()
-    GPIO.output(7, True)
+    #then send the command we want
+    while True :
+        ip
+        dns
+        choice
+        print("-------------------------------------------")
+        print("1. Tester la connectivitée réseau.")
+        print("2. Tester le DNS.")
+        print("3. Table de routage de la sonde.")
+        print("-------------------------------------------")
 
-    if (instruction == 'recv'):
-        GPIO.output(16, True) #Témoin de réception
-        receiveMorse(ws)
-    elif (instruction == 'send'):
-        sendMorse(ws)
-        
+        choice = raw_input()
+
+        if (choice == "1") :
+            print("Entrez une IP")
+            print("-------------")
+            ip = raw_input()
+            print(ip);
+            if isValidIP(ip):
+                self.write_message('ping')
+                print (ws.recv())
+            else: print("l'IP entrée n'est pas valide")
+
+            elif (choice == "2") :
+                print("Entrez une IP ou un nom de domaine")
+                print("----------------------------------")
+                dns = raw_input()
+                print(dns);
+                self.write_message('dns')
+                print (ws.recv())
+
+                elif(choice == "3") :
+                  print("Table de routageR")
+                  self.write_message('routing_table')
+                  print (ws.recv())
+                else :
+                  printf("Entrez 1, 2 ou 3.")
--- serveur.py
+++ serveur.py
@@ -1,4 +1,5 @@
-#!/usr/bin/env python 
+#!/usr/bin/env python
+# coding: utf8
 
 import tornado.web
 import tornado.websocket
@@ -7,87 +8,55 @@ import tornado.ioloop
 import signal
 import sys
 import os
+import socket
+import subprocess
 
 port = 50000
 host = '10.0.0.1'
 
-################ DEFINITION DE LA PATROUILLE 
-pat = "LION"
 
-send_mode = ""
 
 class WebSocketHandler(tornado.websocket.WebSocketHandler):
-    def open(self):
-    	
-    	#switch mode
-    	def switch_transmission_mode(frame, signums):
-    		global send_mode
-        	self.write_message('switch')
-        	send_mode = not send_mode
-        	print ('APPUYER SUR ENTREE')
+    def ping(host):
+        con = os.system('ping -c 1 ' + host)
+        if (con == 0):
+            return True
+        else:
+            return False
 
-  	signal.signal(signal.SIGTSTP, switch_transmission_mode)
+    def dns(domain):
+        return socket.gethostbyname(domain)
+
+    def routes():
+		process = subprocess.Popen(['route', '-n'], stdout=subprocess.PIPE)
+		out, err = process.communicate()
+		return(out)
 
     def on_message(self, message):
-    	global send_mode
-        if (message not in {'.', '_', 'slash'} ): print (message)
-        if (message == 'Hello'):
-            print("Le client demande quoi faire! ")
-            print("   e : lui envoyer du morse.")
-            print("   r : les laisser envoyer du morse.")
-            while True :
-            	order = raw_input()
-            	if (order == 'e') :
-            		send_mode = True; 
-                	self.write_message('recv')
-                	break
-            	elif (order == 'r') :
-            		send_mode = False; 
-                	self.write_message('send')
-                	break
-                else :
-                	print("Attention mauvaise saisie! ")
-                
-        elif (message == 'ready to rcv'):
-        	self.sendMorse()      	
+        print message
+        ping_flag = False
+		dns_flag = False
+		print (message)
+        if message == 'Hello':
+            print("La sonde est connectée. Attente d'une commande.")
 
-       	elif (message == 'ready to send'):
-       		print("LES SCOUTS TRANSMETTENT")
-        	print('CTRL+Z reprendra la main sur la transmission')
-        
-        else :
-        	if (message == 'slash') :
-        		print ('/')
-        	else :
-        		print (message),
-        	self.write_message('ack')
+        elif message == 'ping':
+            ping_flag = True
+        elif (ping_flag is True) and (isValidIP(message)):
+            self.write(ping(message))
+            ping_flag = False
 
+        elif message == 'dns':
+            dns_flag = True
+        elif dns_flag is True and isValidDomaine(message):
+            self.write(dns(message))
+            dns_flag = False
 
+        elif message == 'routing_table':
+            self.write(routes())
+        else:
+            print ('unknown command')
 
-    #send
-    def sendMorse(self):
-    	global send_mode
-    	print("OK c'est parti ! a = TI, e = TA, s = SLASH, q = quitter.")
-        print('CTRL+Z proposera aux scouts de repondre')
-        char = ""
-        while (char != 'q' and send_mode == True):
-            char = raw_input()
-            if char == 'a' :
-                self.write_message('ti')
-                print ('.'),
-            elif char == 'e' :
-                self.write_message('ta')
-                print ('_'),
-            elif char == 's' :
-                self.write_message('slash')
-                print ('/'),
-            elif char == 'q' :
-                self.write_message('q')
-            else :
-                print ('invalid key')
-
-    def on_close(self):
-        exit(0)
 
 class Application(tornado.web.Application):
     def __init__(self):
@@ -96,30 +65,11 @@ class Application(tornado.web.Application):
         ]
         tornado.web.Application.__init__(self, handlers)
 
-  
-  
+
 if __name__ == '__main__':
-
-    if (sys.argv[1] == "lion") :
-        sys.stdout.write("\x1b]2;"+'LION'+"\x07")
-        os.system("setterm -background red -foreground white")
-        port = 50002
-    elif (sys.argv[1] == "gerfaut") :
-        sys.stdout.write("\x1b]2;"+'GERFAUT'+"\x07")
-        os.system("setterm -background blue -foreground black")
-        port = 50003
-    elif (sys.argv[1] == "guepard") :
-        sys.stdout.write("\x1b]2;"+'GUEPARD'+"\x07")
-        os.system("setterm -background black -foreground yellow")
-        port = 50004
-    else :
-        print ('usage : python server.py {lion|gerfaut|guepard}')
-        exit(1)
-
     ws_app = Application()
     server = tornado.httpserver.HTTPServer(ws_app)
     server.listen(port, host)
     tornado.ioloop.IOLoop.instance().start()
 
     exit(0)
-        
